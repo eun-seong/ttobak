819448d2f2f17ee44f984128016b32aad9f97e32
Marbleling <ljiho1998@naver.com>
2020-07-31 01:53:39 +0900

    Add routing for API request

--- audio/server/app.js
+++ audio/server/app.js
@@ -5,20 +5,49 @@ var formidable = require('formidable');
 
 var server = http.createServer((request,response) => { 
 	console.log(request.method, request.url);
-	if (request.method === 'POST' && request.url === '/submit') {
+	if (request.method === 'POST' && request.url === '/api/score') {
 		const form = formidable();
 
   	form.parse(request, (err, fields, files) => {
-  		const transcript = fields.transcript;
+  		try {
+  			const course = fields.course;
+	  		const user = fields.user;
 
-  		const oldpath = files.file.path;
-  		const newpath = 'temp/' + files.file.name;
+	  		const oldpath = files.file.path;
+	  		const newpath = 'temp/' + files.file.name;
 
-  		fs.rename(oldpath, newpath, (err) => {
-  			if (err) throw err;
+	  		fs.rename(oldpath, newpath, (err) => {
+	  			if (err) throw err;
+	  			response.writeHead(200, {"Content-Type": "application/json"});
+	  			response.end(JSON.stringify({course: course, user: user, status: 'ok', score: 55.0}));
+	  		});
+  		} catch (TypeError) {
   			response.writeHead(200, {"Content-Type": "application/json"});
-  			response.end(JSON.stringify({transcript: transcript, file: files.file.name, score: 55.0}));
-  		})
+	  		response.end(JSON.stringify({status: 'fail'}));
+  		}
+
+  		
+  	});
+	} else if (request.method === 'POST' && request.url === '/submit') {
+		const form = formidable();
+
+  	form.parse(request, (err, fields, files) => {
+  		try {
+  			const transcript = fields.transcript;
+
+	  		const oldpath = files.file.path;
+	  		const newpath = 'temp/' + files.file.name;
+
+	  		fs.rename(oldpath, newpath, (err) => {
+	  			if (err) throw err;
+	  			response.writeHead(200, {"Content-Type": "application/json"});
+	  			response.end(JSON.stringify({transcript: transcript, file: files.file.name, score: 55.0}));
+	  		});
+  		} catch (TypeError) {
+  			response.writeHead(200, {"Content-Type": "application/json"});
+	  		response.end(JSON.stringify({status: 'fail'}));
+  		}
+  		
   	});
 	} else if (request.url === '/') {
 		response.writeHead(200, {"Content-Type": "text/html"});
