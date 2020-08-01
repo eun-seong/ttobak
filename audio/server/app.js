0a9ec77fc8a15a7f8bf5ba34f8929735e90738e0
Marbleling <ljiho1998@naver.com>
2020-08-01 23:12:17 +0900

    API request for updating data added

--- audio/server/app.js
+++ audio/server/app.js
@@ -4,9 +4,53 @@ var qs = require('querystring');
 var formidable = require('formidable');
 var shell = require('shelljs');
 
+const responseError = (response, request) => {
+	response.writeHead(200, {"Content-Type": "application/json"});
+	response.end(JSON.stringify({status: 'fail'}));
+}
+
 var server = http.createServer((request,response) => { 
 	console.log(request.method, request.url);
-	if (request.method === 'POST' && request.url === '/api/score') {
+
+	if (request.method === 'POST' && request.url === '/api/update/user') {
+		const form = formidable();
+
+  	form.parse(request, (err, fields, files) => {
+  		try {
+  			const uid = fields.uid;
+	  		const user = fields.user;
+	  		const gender = fields.gender;
+
+	  		const line = `${uid}|${user}|${gender}\n`;
+
+	  		fs.appendFile('../../data/AUDIO_INFO', line, function (err) {
+	  			if (err) throw err;
+
+	  			response.writeHead(200, {"Content-Type": "application/json"});
+  				response.end(JSON.stringify({request: '/api/update/user', status: 'ok'}));
+	  		})
+  		} catch (Error) {
+  			responseError(response, '/api/update/user');
+  		}
+  	});
+	} else if (request.method === 'POST' && request.url === '/api/update/transcript') {
+		const form = formidable();
+
+  	form.parse(request, (err, fields, files) => {
+  		try {
+  			const transcript = fields.transcript;
+  			const course = fields.course;
+
+	  		if (shell.exec(`cd ../scripts; python3 updateData "${transcript}" ../../model/lm ../../data/${course}`).code !== 0) throw "ExecutionError";
+
+  			response.writeHead(200, {"Content-Type": "application/json"});
+				response.end(JSON.stringify({request: '/api/update/transcript', status: 'ok'}));
+  		} catch (Error) {
+  			responseError(response, '/api/update/transcript');
+  		}
+  		
+  	});
+	} else if (request.method === 'POST' && request.url === '/api/score') {
 		const form = formidable();
 
   	form.parse(request, (err, fields, files) => {
@@ -20,26 +64,25 @@ var server = http.createServer((request,response) => {
 
 	  		fs.rename(oldpath, newpath, (err) => {
 	  			if (err) throw err;
-	  			if (shell.exec('cd ..; ./audio2pron.sh ' + course + ' ' + user + ' ' + files.file.name).code === 0) {
-	  				fs.readFile(resultpath, 'utf8', (err, data) => {
-	  					response.writeHead(200, {"Content-Type": "application/json"});
-	  					response.end(JSON.stringify({course: course, user: user, status: 'ok', score: data}));
-	  				});
-	  			} else {
-	  				response.writeHead(200, {"Content-Type": "application/json"});
-	  				response.end(JSON.stringify({status: 'fail'}));
-	  			}
+	  			if (shell.exec(`cd ..; ./audio2pron.sh ${course} ${user} ${files.file.name}`).code !== 0) throw "ExecutionError";
+
+	  			fs.readFile(resultpath, 'utf8', (err, data) => {
+  					if (err) throw err;
+
+  					response.writeHead(200, {"Content-Type": "application/json"});
+  					response.end(JSON.stringify({request: '/api/score', course: course, user: user, status: 'ok', score: data}));
+  					
+  				});
 
 	  			
 	  		});
-  		} catch (TypeError) {
-  			response.writeHead(200, {"Content-Type": "application/json"});
-	  		response.end(JSON.stringify({status: 'fail'}));
+  		} catch (Error) {
+  			responseError(response, '/api/score');
   		}
 
   		
   	});
-	} else if (request.method === 'POST' && request.url === '/submit') {
+	} else if (request.method === 'POST' && request.url === '/api/segscore') {
 		const form = formidable();
 
   	form.parse(request, (err, fields, files) => {
@@ -52,21 +95,18 @@ var server = http.createServer((request,response) => {
 
 	  		fs.rename(oldpath, newpath, (err) => {
 	  			if (err) throw err;
-	  			if (shell.exec('cd ..; ./seg_and_audio2pron.sh "' + transcript + '" ' + files.file.name).code === 0) {
-	  				fs.readFile(resultpath, 'utf8', (err, data) => {
-	  					response.writeHead(200, {"Content-Type": "application/json"});
-	  					response.end(JSON.stringify({status: 'ok', score: data}));
-	  				});
-	  				
-	  			} else {
-	  				response.writeHead(200, {"Content-Type": "application/json"});
-	  				response.end(JSON.stringify({status: 'fail'}));
-	  			}
+	  			if (shell.exec(`cd ..; ./seg_and_audio2pron.sh "${transcript}" ${files.file.name}`).code !== 0) throw "ExecutionError";
+
+	  			fs.readFile(resultpath, 'utf8', (err, data) => {
+  					if (err) throw err;
+
+  					response.writeHead(200, {"Content-Type": "application/json"});
+  					response.end(JSON.stringify({request: '/api/segscore', status: 'ok', score: data}));
+  				});
 	  			
 	  		});
-  		} catch (TypeError) {
-  			response.writeHead(200, {"Content-Type": "application/json"});
-	  		response.end(JSON.stringify({status: 'fail'}));
+  		} catch (Error) {
+  			responseError(response, '/api/segscore');
   		}
   		
   	});
