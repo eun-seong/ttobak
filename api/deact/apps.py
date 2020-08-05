c0d137f8752d7df5ed688a578afae745fe983883
조동혁 <joeastgur@korea.ac.kr>
2020-08-05 12:03:10 +0000

    DRF basic structure

---
+++ api/deact/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DeactConfig(AppConfig):
+    name = 'deact'
---
+++ api/deact/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Post(models.Model):
+    content = models.TextField()
+
+    def __str__(self):
+        return self.content
+# Create your models here.
