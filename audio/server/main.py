1284a1fde6299d596d8758f1b2a2a7ad72e65ebb
이지호 <ljiho1998@naver.com>
2020-08-10 20:29:30 +0900

    Added Sanic Server Settings

---
+++ audio/server/main.py
@@ -0,0 +1,154 @@
+from sanic import Sanic
+from sanic import response
+from sanic.response import json
+
+import subprocess
+import time
+import os
+
+app = Sanic(__name__)
+
+ALLOWED_EXTENSIONS = set(['mp3', 'm4a', 'flac', 'wav', 'aac', 'ogg', 'wma', 'aac', '3gp'])
+
+PATH = {
+    'PATH': "/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin"
+}
+
+ERROR_CODE = {
+    0: 'Success',
+    1: 'Insufficient Parameters',
+    2: 'Parameter is not filled',
+    3: 'Invalid extensions',
+    4: 'Processing error',
+    5: 'Invalid Parameters',
+}
+
+
+@app.route('/')
+async def index(request):
+    return await response.file('index.html')
+
+@app.route('/api')
+async def api_index(request):
+    return await response.file('api.html')
+
+@app.route('/api/user/update', methods=['POST'])
+async def update_user(request):
+    if "uid" not in request.form or \
+        "name" not in request.form or \
+        "gender" not in request.form:
+        return error(request.path, 1)
+
+    uid = request.form['uid']
+    name = request.form['name']
+    gender = request.form['gender']
+
+    if uid == '' or name == '' or gender == '':
+        return error(request.path, 2)
+    if gender != 'm' and gender != 'f':
+        return error(request.path, 5)
+
+    line = '{}|{}|{}\n'.format(uid, name, gender)
+
+    audio_info = open('../../data/AUDIO_INFO', 'a')
+    audio_info.write(line)
+    audio_info.close()
+
+    return json({'request': request.path, 'status': 'Success'})
+
+@app.route('/api/transcript/update', methods=['POST'])
+async def update_transcript(request):
+    if 'transcript' not in request.form or \
+        'course' not in request.form:
+        return error(request.path, 1)
+
+    transcript = request.form['transcript']
+    course = request.form['course']
+
+    if transcript == '' or course == '':
+        return error(request.path, 2)
+
+    lm = '../../model/lm'
+    target = '../../data/{}'.format(course)
+    try:
+        subprocess.run('python3 makeData.py "{}" {} {}'.format(transcript, lm, target), shell=True, check=True, cwd=os.path.abspath('../scripts'))
+    except:
+        return error(request.path, 4)
+
+    return json({'request': request.path, 'status': 'Success'})
+
+@app.route('/api/score', methods=['POST'])
+async def score(request):
+    if "course" not in request.form or \
+        "user" not in request.form or \
+        "file" not in request.files:
+        return error(request.path, 1)
+
+    course = request.form['course']
+    user = request.form['user']
+    file = request.files['file']
+
+    if course == '' or user == '':
+        return error(request.path, 2)
+
+    filename = make_filename(file.filename)
+    if filename == '':
+        return error(request.path, 3)
+
+    file.save('temp/raw/' + filename)
+
+    subprocess.run('./audio2pron.sh {} {} {}'.format(course, user, filename), shell=True, cwd=os.path.abspath('..'))
+
+    if not os.path.isfile('temp/result/{}.txt'.format(filename)):
+        return error(request.path, 4)
+
+    result_file = open('temp/result/{}.txt'.format(filename), 'r')
+    result = result_file.readline().strip()
+    result_file.close()
+
+    return json({'request': request.path, 'status': 'Success', 'score': float(result)})
+
+@app.route('/api/segscore', methods=['POST'])
+async def seg_score(request):
+    if 'transcript' not in request.form or \
+        'file' not in request.files:
+        return error(request.path, 1)
+
+    transcript = request.form['transcript']
+    file = request.files['file']
+
+    if transcript == '':
+        return error(request.path, 2)
+
+    filename = make_filename(file.filename)
+    if filename == '':
+        return error(request.path, 3)
+
+    file.save('temp/raw/' + filename)
+
+    subprocess.run('./seg_and_audio2pron.sh "{}" {}'.format(transcript, filename), shell=True, cwd=os.path.abspath('..'))
+
+    if not os.path.isfile('temp/result/{}.txt'.format(filename)):
+        return error(request.path, 4)
+
+    result_file = open('temp/result/{}.txt'.format(filename), 'r')
+    result = result_file.readline().strip()
+    result_file.close()
+
+    return json({'request': request.path, 'status': 'Success', 'score': float(result)})
+
+
+def make_filename(filename):
+    millis = int(round(time.time() * 1000))
+    ext = filename.rsplit('.', 1)[1]
+
+    if ext not in ALLOWED_EXTENSIONS:
+        return ''
+
+    return '{}.{}'.format(str(millis), ext)
+
+def error(req, err):
+    return json({'request': req, 'status': 'Fail', 'error': ERROR_CODE[err]})
+
+if __name__ == '__main__':
+    app.run()
