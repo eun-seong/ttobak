bbeaee2c6bde2ab628477627f6be6e4fbd9d708a
marble25 <ljiho1998@naver.com>
2020-08-29 17:16:06 +0900

    Learning Script for Zeroth Added

--- audio/install.sh
+++ audio/install.sh
@@ -4,6 +4,8 @@
 
 cp config.sh $ZEROTH_ROOT/config.sh
 
+cp run_custom.sh $ZEROTH_ROOT/run_custom.sh
+
 cp calc_pron_score.py $ZEROTH_ROOT/local/calc_pron_score.py
 cp result2phone.py $ZEROTH_ROOT/local/result2phone.py
 cp result2time.py $ZEROTH_ROOT/local/result2time.py
