62e3ed44380e7a45100c9013ba1b80e7cce4e408
marble25 <ljiho1998@naver.com>
2020-09-09 10:53:34 +0900

    스크립트 수정

--- contents/makeTotal.py
+++ contents/makeTotal.py
@@ -1,10 +1,15 @@
 import glob
 files = glob.glob('*/*/*.txt')
-files = [el for el in files if 'readme' not in el]
+files = [el for el in files if 'readme' not in el and 'noise' not in el]
+
+noise_files = glob.glob('*/*/*.txt')
+noise_files = [el for el in noise_files if 'noise' in el]
 
 files.sort()
+noise_files.sort()
 
 results = {}
+noises = {}
 
 for txt in files:
     with open(txt, 'r') as f:
@@ -20,6 +25,19 @@ for txt in files:
 with open('total.txt', 'w') as f:
     for el in results:
         f.write('{} {}\n'.format(el, results[el]))
+
+for txt in noise_files:
+    with open(txt, 'r') as f:
+        lines = f.readlines()
+        lines = [el for el in lines if el != '\n']
+        for idx, line in enumerate(lines):
+            key = '{0}_{1:04d}'.format(txt.split('.')[0], idx+1)
+            print(key + ' ' + line.strip())
+            noises[key] = line.strip()
+
+with open('noises.txt', 'w') as f:
+    for el in noises:
+        f.write('{} {}\n'.format(el, noises[el]))
 
 values = list(set(results.values()))
 with open('script.txt', 'w') as f:
