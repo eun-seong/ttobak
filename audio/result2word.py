97c8723309cb1443ec2792fc2b5c5fcc50af2974
marble25 <ljiho1998@naver.com>
2020-09-29 11:49:45 +0900

    주석 추가

--- audio/result2word.py
+++ audio/result2word.py
@@ -58,23 +58,25 @@ def parse_text(src, dst):
 
     for i in range(1, len(seq)):
         if seq[i] == '' or seq[i] == '\n': continue
-        elif seq[i] == 'SIL': 
+        elif seq[i] == 'SIL': # 띄어쓰기일 경우
             phone_word_idx+=1
             phone_word_list.append([])
-        else: phone_word_list[phone_word_idx].append(seq[i].split('_')[0])
+        else: phone_word_list[phone_word_idx].append(seq[i].split('_')[0]) 
     input_file.close()
 
     word_list = []
     for word_idx in range(len(phone_word_list)):
         current_idx = 0
         word_list.append('')
-        if len(phone_word_list[word_idx]) == 0:
+        if len(phone_word_list[word_idx]) == 0: # 빈 array이면 넘어감
             continue
 
         while(True):
-            if current_idx >= len(phone_word_list[word_idx]): break
+            if current_idx >= len(phone_word_list[word_idx]): break # 현재 인덱스가 array 크기 초과하면 넘어감
 
             el = phone_word_list[word_idx][current_idx]
+
+            # 자음 없이 모음으로 시작하는 경우를 감지
             flag = False
             for mo in 'aeiouw':
                 if mo in el:
@@ -86,6 +88,7 @@ def parse_text(src, dst):
                 cho = MAPPING_TABLE[phone_word_list[word_idx][current_idx]]
                 current_idx += 1
 
+            # 다음 글자가 마지막이고 그 글자가 종성이거나, 다음 글자가 마지막이 아닌데 종성 초성이 연달아 나온다면 받침이 있는 경우
             if (current_idx + 2 == len(phone_word_list[word_idx]) \
                     and MAPPING_TABLE[phone_word_list[word_idx][current_idx+1]] in JONGSUNG) \
                 or (current_idx + 2 < len(phone_word_list[word_idx]) \
@@ -100,7 +103,7 @@ def parse_text(src, dst):
 
                 word_list[word_idx] += chr(0xAC00 + ((cho_idx*21)+jung_idx)*28+jong_idx)
                 current_idx += 2
-            else:
+            else: # 받침이 없는 경우
                 jung = MAPPING_TABLE[phone_word_list[word_idx][current_idx]]
                 cho_idx = CHOSUNG.index(cho)
                 jung_idx = JUNGSUNG.index(jung)
@@ -109,7 +112,8 @@ def parse_text(src, dst):
                 current_idx += 1
     word_list = [el for el in word_list if el != '']
     
-    f = open('word.txt', 'w')
+    # 파일로 저장
+    f = open(dst, 'w')
     for word in word_list:
         f.write(word + ' ')
     f.close()
